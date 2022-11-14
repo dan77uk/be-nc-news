953f8ad28b3bf7410175d7411f82271c22486c62
Dan <dan.phillips100@gmail.com>
2022-11-14 18:08:35 +0000

    4. GET /api/articles - complete

--- __tests__/getArticles.test.js
+++ __tests__/getArticles.test.js
@@ -20,4 +20,35 @@ describe("GET /api/topics", () => {
         expect(body.articles).toHaveLength(12);
       });
   });
+
+  it("array should be sorted by created_at column in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toBeSorted({
+          key: "created_at",
+          descending: true,
+        });
+      });
+  });
+
+  it("should return an array of articles with all required properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            author: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
 });
--- models/selectArticles.js
+++ models/selectArticles.js
@@ -1,8 +1,30 @@
 const db = require("../db/connection");
 
 exports.selectArticles = () => {
-  const queryStr = `SELECT * FROM articles;`;
-  return db.query(queryStr).then((result) => {
-    return result.rows;
-  });
+  return db
+    .query("ALTER TABLE articles ADD COLUMN comment_count INT DEFAULT 0;")
+    .then(() => {
+      return db.query(
+        "SELECT article_id, COUNT(article_id)::INT as count FROM comments GROUP BY article_id;"
+      );
+    })
+    .then(({ rows }) => {
+      const update = rows.map(({ article_id, count }) => {
+        const queryStr = `UPDATE articles SET comment_count = $1 WHERE article_id = $2`;
+        return db.query(queryStr, [count, article_id]);
+      });
+      return Promise.all(update).then(() => {
+        return db
+          .query(`SELECT * FROM articles ORDER BY created_at DESC;`)
+          .then((result) => {
+            if (result.rows.length === 0) {
+              return Promise.reject({
+                status: 204,
+                msg: "No Content",
+              });
+            }
+            return result.rows;
+          });
+      });
+    });
 };
