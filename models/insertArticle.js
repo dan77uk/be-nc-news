7f1a34529d982bf9ec03e337032a3149749c83d8
Dan <dan.phillips100@gmail.com>
2022-11-18 17:40:00 +0000

    Removed unrequired result from callback

--- models/insertArticle.js
+++ models/insertArticle.js
@@ -17,18 +17,20 @@ exports.insertArticle = (postedBody) => {
   }
 
   return Promise.all([checkUserExists(username), checkTopicExists(topic)]).then(
-    (result) => {
+    () => {
       return db
         .query(
           "INSERT INTO articles (author, body, title, topic) VALUES ($1, $2, $3, $4) RETURNING article_id;",
           [username, body, title, topic]
         )
         .then((result) => {
-          return db.query(`SELECT articles.*,
-    COUNT(comments.comment_id)::INT AS comment_count
-    FROM articles
-    LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = ${result.rows[0].article_id}
-    GROUP BY articles.article_id;`);
+          return db.query(`
+            SELECT articles.*,
+            COUNT(comments.comment_id)::INT AS comment_count
+            FROM articles
+            LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = ${result.rows[0].article_id}
+            GROUP BY articles.article_id;
+          `);
         })
         .then((result) => {
           return result.rows[0];
